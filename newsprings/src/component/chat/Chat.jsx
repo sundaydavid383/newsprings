1f0e952aec34c456f184c69033e3307948484b22
sundaydavid383 <sundayudoh383@gmail.com>
2025-05-12 18:08:55 +0100

    chat new commit

--- newsprings/src/component/chat/Chat.jsx
+++ newsprings/src/component/chat/Chat.jsx
@@ -1,11 +1,12 @@
 
 import React, { useState } from 'react';
 import axios from "axios";
+import "./chat.css"
 
 const Chat = () => {
     const [seeForm, setSeeForm] = useState(false)
     const [query, setQuery] = useState("");
-    const [response, setResponse] = useState("");
+    const [response, setResponse] = useState("what is going on i dont understand");
     const openRouterApiKey = "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";
     const sendMessage = async (message) => {
         try {
@@ -25,12 +26,12 @@ const Chat = () => {
               ],
             },
             {
-              headers: {
-                "Authorization": `Bearer ${openRouterApiKey}`,
-                "Content-Type": "application/json",
-                "HTTP-Referer": "https://newsprings-raqf.vercel.app/", // your actual domain
-                "X-Title": "RCCG Newspings Chatbot"
-              },
+                headers: {
+                    "Authorization": `Bearer ${openRouterApiKey}`,
+                    "Content-Type": "application/json",
+                    "HTTP-Referer": "https://your-vercel-app-name.vercel.app", // must match your deployed frontend
+                    "X-Title": "RCCG Newspings Chatbot"
+                  },
             }
           );
       
@@ -42,12 +43,14 @@ const Chat = () => {
       };
   return (
     <div>
-        {!seeForm ? <div onClick={()=>setSeeForm(true)} className='ai iconactive message_state'>
-            <div className="words">hello how are you doing</div>
-            <i className="fa-solid fa-robot"></i>qw
-        </div>
+        {!seeForm ? 
+        <div className='bot_state'>
+        <div className="chat_bot_words">hello how are you doing</div>
+    <i onClick={()=>setSeeForm(true)} className='chat iconactive chat_state fa-solid fa-robot'></i>
+     </div>   
         :(
         <div className='chat_box_holder'>
+                        <i onClick={()=>setSeeForm(false)} className="fa-solid fa-times"></i>
             {response && <div className="bot-reply">{response}</div>}
 
 <form
@@ -60,7 +63,7 @@ const Chat = () => {
     setQuery(""); // Clear input
   }}
 >
-            <i onClick={()=>setSeeForm(false)} className="fa-solid fa-times"></i>
+
             <input
   type="text"
   placeholder="Ask newsprings anything"
